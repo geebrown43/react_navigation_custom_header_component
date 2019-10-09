fa58a69144d50c9ba571673c7e56f1bbd7ccf2c9
Gavin Brown <gavindevelops@gmail.com>
2019-10-09 18:50:08 -0500

    removed unnecessary component

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React from 'react';
 import { createAppContainer} from 'react-navigation'
 import AppNav from './src'
@@ -13,9 +5,8 @@ import AppNav from './src'
 const AppNavigator = createAppContainer(AppNav);
 
 
-const App: () => React$Node = () => {
-  return <AppNavigator/> ;
-};
+const App = () => <AppNavigator/> ;
+
 
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { View, Text, Dimensions } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 
+// Screen to be displayed
 class HomeScreen extends React.Component {
     render() {
         return (
-            <View>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
                 <Text> This is my Home Screen </Text>
             </View>
         )
@@ -13,9 +14,11 @@ class HomeScreen extends React.Component {
 } 
 const { width, height } = Dimensions.get('screen')
 
+// Custom Header Component 
+
 const CustomHeaderComponent = ({ navBackgroundColor = 'lightgrey', navLeftText, navRightText, customComponent, customComponentPosition, navBarShadowColor = 'grey', navBarShadowOffset = {width: 1, height: 1}, navBarShadowOpacity = 1 }) => {
     return (
-        <View style={{width, height: height / 8, backgroundColor:navBackgroundColor, shadowColor:navBarShadowColor, shadowOffset:navBarShadowOffset, shadowOpacity:navBarShadowOpacity, alignItems: 'center', flexDirection: "row", justifyContent: 'space-around'}}>
+        <View style={{width, height: height / 8, backgroundColor:navBackgroundColor, shadowColor:navBarShadowColor, shadowOffset:navBarShadowOffset, shadowOpacity:navBarShadowOpacity, alignItems: 'center', flexDirection: "row", justifyContent: 'space-around', }}>
             {navLeftText ? <Text>{navLeftText}</Text> : null}
             {customComponent ? customComponent : null}
             {navRightText ? <Text>{navRightText}</Text> : null}
@@ -23,6 +26,7 @@ const CustomHeaderComponent = ({ navBackgroundColor = 'lightgrey', navLeftText, 
     )
 }
 
+// Imported this component in App.js
 const AppNavigator = createStackNavigator({
     Home: {
         screen: HomeScreen,
